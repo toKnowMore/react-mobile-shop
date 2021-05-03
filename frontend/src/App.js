46a11081cf195dd803e3e57e6478416856bbd68c
zhufukun <247918066@qq.com>
2021-05-03 16:56:02 +0800

    header footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </main>
+     
+      <Footer></Footer>
     </>
   );
 }
