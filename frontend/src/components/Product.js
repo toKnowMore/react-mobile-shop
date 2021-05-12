b842d0ebe1ee7ddc68f53c125e3892adde08c09f
zhufukun <2479188066@qq.com>
2021-05-12 15:03:04 +0800

    react路由

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 import Rating from './Rating'
 
 const Product = ({product}) => {
     return (
         <Card className="my-3 py-3 rounded">
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant="top"></Card.Img>
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title>{product.name}</Card.Title>
-                </a>
+                </Link>
                 <Card.Text as="div">
                     <Rating value={product.rating} text={`${product.numReviews}条评论`}></Rating>  
                 </Card.Text>
