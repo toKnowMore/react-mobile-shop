d9a2d8aabca3363b14626271f2d5f443430eae36
zhufukun <2479188066@qq.com>
2021-05-12 15:38:22 +0800

    产品详情

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,61 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import products from '../products'
+import Rating from '../components/Rating'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+    const product = products.find(x => x._id === match.params.id)
     return (
-        <div>
-            产品详情
-        </div>
+        <>
+            <Link className='btn btn-dark my-3' to='/'>返回主页</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid></Image>
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews}条评论`}></Rating>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            价格: ¥{product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            描述: ¥{product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>价格:</Col>
+                                    <Col>
+                                        <strong> ¥{product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>库存:</Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? '有货' : '无货'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button className='btn-block' disabled={product.countInStock === 0}>添加到购物车</Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
     )
 }
 
