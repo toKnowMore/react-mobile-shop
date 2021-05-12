cca2ead1ff35a92055cf1d52f2a83b14bbaebf44
zhufukun <2479188066@qq.com>
2021-05-12 17:06:20 +0800

    axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,34 @@
-import React from 'react'
-import products from '../products'
-import {Row, Col} from 'react-bootstrap'
-import Product from '../components/Product'
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
-    return (
-        <>
-            <h1>最新产品</h1>
-            <Row>
-                {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product}/>
-                    </Col>
-                )) }
-            </Row>
-        </>
-    )
-}
+  const [products, setProducts] = useState([]);
 
-export default HomeScreen
+  // 类似于声明周期
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      // 赋值
+      setProducts(data)
+    };
+
+    fetchProducts();
+  }, []);
+  return (
+    <>
+      <h1>最新产品</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
